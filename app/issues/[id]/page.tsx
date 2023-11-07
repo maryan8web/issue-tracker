85681182dfdd519fb996859be3f16d17c5c089b2
maryan8web <mats.maryan.dev@gmail.com>
2023-11-07 09:51:28 +0200

    Disable SSR for loading markdown editor

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -19,14 +19,14 @@ const IssueDetailPage = async ({ params }: Props) => {
   }
 
   return (
-    <div>
+    <div className="max-w-xl">
       <Heading>{issue.title}</Heading>
-      <Flex gap="3" my="2">
+      <Flex className="space-x-3" my="2">
         <IssueStatusBadge status={issue.status} />
 
         <Text>{issue.createdAt.toDateString()}</Text>
       </Flex>
-      <Card className="pros" mt="4">
+      <Card className="prose" mt="4">
         <ReactMarkdown>{issue.description}</ReactMarkdown>
       </Card>
     </div>
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -6,8 +6,8 @@ import {
   Text,
   TextField,
 } from "@radix-ui/themes";
+import dynamic from "next/dynamic";
 import React, { useState } from "react";
-import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { useForm, Controller } from "react-hook-form";
 import axios from "axios";
@@ -17,6 +17,10 @@ import { createIssueSchema } from "@/app/validationSchemas";
 import { z } from "zod";
 import ErrorMessage from "@/app/components/ErrorMessage";
 import Spinner from "@/app/components/Spinner";
+
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
