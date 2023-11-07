887a51c1a3267f1103f8836ab55b439b1a837264
maryan8web <mats.maryan.dev@gmail.com>
2023-11-07 12:29:22 +0200

    Fix caching issues

--- app/issues/_components/IssueForm.tsx
+++ app/issues/_components/IssueForm.tsx
@@ -45,6 +45,7 @@ const IssueForm = ({ issue }: Props) => {
         await axios.post("/api/issues", data);
       }
       router.push("/issues");
+      router.refresh();
     } catch (error) {
       setIsSubmitting(false);
       setError("An unexpected error");
